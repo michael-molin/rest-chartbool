4519c18f5183393138be95504cf60e75c3366ad7
Michael <m.molin.1987@gmail.com>
2020-04-08 13:42:15 +0200

    Pulizia struttura codice, Milestone 1 completata

--- js/main.js
+++ js/main.js
@@ -1,10 +1,94 @@
 $(document).ready(function () {
 
-    richiamaDati();
+    richiamaGet();
 
+    function richiamaGet() {
+        $.ajax({
+            url: "http://157.230.17.132:4023/sales",
+            method: "GET",
+            success: function (data) {
+                var datiGrezzi = ottieniDatiGrezzi(data);
 
-    function creaGrafico(mese, vendite) {
-        var ctx = $('#grafico-annuale');
+                var datiGraficoUno = creaDatiGraficoUno(datiGrezzi);
+                creaGraficoUno(datiGraficoUno.mese, datiGraficoUno.vendite);
+
+                var datiGraficoDue = creaDatiGraficoDue(datiGrezzi);
+                creaGraficoDue(datiGraficoDue.venditori, datiGraficoDue.vendite);
+            },
+            error: function() {
+                alert('ERRORE');
+            }
+        })
+    }
+
+    function ottieniDatiGrezzi(data) {
+        var datiVendite = data;
+        var annoVendite = [];
+        for (var i = 0; i < datiVendite.length; i++) {
+            var datoVendita = datiVendite[i];
+            var dataVendita = moment(datoVendita.date , "L");
+            var elementoVendita = {
+                id: datoVendita.id,
+                venditore: datoVendita.salesman,
+                indiceMese: dataVendita.format("MM"),
+                mese: dataVendita.format("MMMM"),
+                numeroVendite: datoVendita.amount
+            }
+            annoVendite.push(elementoVendita);
+        }
+        annoVendite.sort(function(a,b) {
+            return a.indiceMese - b.indiceMese;
+        })
+
+        return annoVendite;
+    }
+
+    function creaDatiGraficoUno(anno) {
+        var venditeAssociate = {};
+        for (var i = 0; i < anno.length; i++) {
+            var meseTemp = anno[i].mese;
+            if (venditeAssociate[meseTemp] === undefined) {
+                venditeAssociate[meseTemp] = 0;
+            }
+            venditeAssociate[meseTemp] += anno[i].numeroVendite;
+        }
+        var arrayMese = [];
+        var arrayVendite = [];
+        for (var key in venditeAssociate) {
+            arrayMese.push(key);
+            arrayVendite.push(venditeAssociate[key]);
+        }
+        return {
+            mese: arrayMese,
+            vendite: arrayVendite
+        }
+    }
+
+    function creaDatiGraficoDue(dati) {
+        var venditeSingole = {};
+        for (var i = 0; i < dati.length; i++) {
+            var questoVenditore = dati[i].venditore;
+            if (venditeSingole[questoVenditore] === undefined) {
+                venditeSingole[questoVenditore] = 0;
+            }
+            venditeSingole[questoVenditore] += dati[i].numeroVendite;
+        }
+
+        var arrayVenditori = [];
+        var arrayVendite = [];
+        for (var key in venditeSingole) {
+            arrayVenditori.push(key);
+            arrayVendite.push(venditeSingole[key]);
+        }
+
+        return {
+            venditori: arrayVenditori,
+            vendite: arrayVendite
+        }
+    }
+
+    function creaGraficoUno(mese, vendite) {
+        var ctx = $('#grafico-vendite-annuale');
         var chart = new Chart(ctx, {
             type: 'line',
             data: {
@@ -20,61 +104,25 @@ $(document).ready(function () {
         });
     }
 
-    function richiamaDati() {
-        $.ajax({
-            url: "http://157.230.17.132:4023/sales",
-            method: "GET",
-            success: function (data) {
-                var datiVendite = data;
-                var annoVendite = [];
-                for (var i = 0; i < datiVendite.length; i++) {
-                    var datoVendita = datiVendite[i];
-                    var dataVendita = moment(datoVendita.date , "L");
-                    var meseVendita = {
-                        numeroMese: dataVendita.format("MM"),
-                        mese: dataVendita.format("MMMM"),
-                        numeroVendite: datoVendita.amount
+    function creaGraficoDue (venditori, vendite) {
+        console.log(venditori);
+        console.log(vendite);
+        // var venditeTotali = 0;
+        // for (var i = 0; i < vendite.length; i++) {
+        //     venditeTotali += vendite[i];
+        // }
+        var ctx = $('#grafico-addetti-vendite');
+        var myPieChart = new Chart(ctx, {
+            type: 'pie',
+            data: {
+                labels: venditori,
 
-                    }
-                    annoVendite.push(meseVendita);
-                }
-                annoVendite.sort(function(a,b) {
-                    return a.numeroMese- b.numeroMese;
-                })
-                console.log(annoVendite);
-
-                creaDatiGrafico(annoVendite);
-            },
-
-            error: function() {
-                alert('ERRORE');
+                datasets: [{
+                    data: vendite,
+                    label: 'Report Addetti Anno 2017',
+                    backgroundColor: ['red', 'blue', 'yellow', 'green']
+                }]
             }
-        })
-    }
-
-    function creaDatiGrafico(anno) {
-        var venditeAssociate = {};
-
-        for (var i = 0; i < anno.length; i++) {
-            var meseTemp = anno[i].mese;
-            if (venditeAssociate[meseTemp] === undefined) {
-                venditeAssociate[meseTemp] = 0;
-            }
-            // console.log('al mese ' + meseTemp + ' aggiungo: ' + anno[i].numeroVendite);
-            venditeAssociate[meseTemp] += anno[i].numeroVendite;
-            // console.log('chiamata numero: ' + i + ' dato: ',venditeAssociate);
-        }
-        // console.log('-----------------------------------------------------');
-        // console.log(venditeAssociate);
-        var arrayMese = [];
-        var arrayVendite = [];
-
-        for (var key in venditeAssociate) {
-            arrayMese.push(key);
-            arrayVendite.push(venditeAssociate[key]);
-        }
-        // console.log(arrayMese);
-        // console.log(arrayVendite);
-        creaGrafico(arrayMese, arrayVendite);
+        });
     }
 });
